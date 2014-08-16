fa20a121b6b07b621fbe523f27cdf9221bdce400
barbosm <barbosm@users.noreply.github.com>
2014-08-16 18:46:03 -0300

    challenge 1, set 1

---
+++ set1/1.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+# Convert hex to base64
+# Convert 49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d
+# to SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t
+
+data = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
+hex_decode = data.decode('hex')
+
+print hex_decode.encode('base64')
